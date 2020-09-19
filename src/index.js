0eab613b53b24860ffea055feddfeebc130a7cf7
Labutkina Yulia <labutkinayuliaa@gmail.com>
2020-09-19 19:39:42 +0300

    added additional task

---
+++ src/index.js
@@ -0,0 +1,81 @@
+import './styles.css';
+import articles from './js/articles-obj';
+import newsArticle from './template/news.hbs';
+
+
+// Фукционал
+//  - Пользователь может ввести свое имя и фамилию в форму и отправить ее
+//  - После отправки, под формой вы должны отрендерить надпись {name} {secondName}, 
+// выбирите категорию новостей а также селектор где пользователь может выбрать 1 из 3 категорий новостей
+//  - После выбора категории новости вы должны отрендерить те новости которые выбрал пользователь 
+//  - Пользователь может выбрать другую категорию, вы должны отрендерить новости из новой категории
+// - При перезагрузке страницы имя пользовател и выбранная категория должна сохранятся 
+
+
+const formRef = document.querySelector('.user-form');
+const userNameRef = document.querySelector('input.name');
+const userSurnameRef = document.querySelector('input.surname');
+const newsListRef = document.querySelector('select.oll-news');
+const newsRef = document.querySelector('div.news');
+
+const createUserName = (userName) => {
+  const element = document.createElement('div');
+  element.textContent = `${userName}`;
+  formRef.after(element);
+}
+
+const clearValue = () => {
+  userNameRef.value = '';
+  userSurnameRef.value = '';
+}
+
+
+const submitForm = (event) => {
+  event.preventDefault();
+  localStorage.setItem('userName', `${userNameRef.value} ${userSurnameRef.value}`);
+  const userName = localStorage.getItem('userName');
+  createUserName(userName);
+  clearValue();
+}
+
+const newsCategory = (obj) => obj.map(el => el.category).filter((el, index, arr) => arr.indexOf(el) === index);
+
+const createNewsCategoryList = () => {
+  const articlesNews = newsCategory(articles);
+  newsListRef.innerHTML = articlesNews.reduce((acc, current, index) => acc + `<option value = "${(current)}">${current}</option>`, '<option value = ""></option>');
+}
+createNewsCategoryList();
+
+
+const createNews = (el) => {
+
+  const news = el.reduce((acc, current) => acc + newsArticle(current), '');
+  newsRef.innerHTML = news;
+}
+
+const arrayNews = () => articles.filter(el => newsListRef.value === el.category);
+
+const renderNews = (event) => {
+
+  localStorage.setItem('category', `${event.target.value}`);
+  createNews(arrayNews());
+}
+
+
+newsListRef.addEventListener('change', renderNews);
+formRef.addEventListener('submit', submitForm);
+
+
+const savedSettings = () => {
+  if(localStorage.getItem('userName')) {
+    createUserName(localStorage.getItem('userName'));
+  }
+
+  if(localStorage.getItem('category')) {
+    newsListRef.value = localStorage.getItem('category');
+    createNews(arrayNews());
+  }
+
+}
+
+savedSettings();
---
+++ src/js/articles-obj.js
@@ -0,0 +1,52 @@
+const articles = [
+    {
+      title: "123",
+      article:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, asperiores optio obcaecati illum nobis aspernatur, suscipit numquam odio quaerat aperiam magni! Aut laboriosam tempore quis placeat esse non impedi aspernatur!",
+      date: "321",
+      comments: [{ comment: 'coment-politics' }, { comment: 'coment-politics' }],
+      category: 'politics'
+    },
+    {
+      title: "123",
+      article:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, asperiores optio obcaecati illum nobis aspernatur, suscipit numquam odio quaerat aperiam magni! Aut laboriosam tempore quis placeat esse non impedi aspernatur!",
+      date: "321",
+      comments: [{ comment: 'coment-politics' }, { comment: 'coment' }, { comment: 'coment-politics' }],
+      category: 'politics'
+    },
+    {
+      title: "123",
+      article:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, asperiores optio obcaecati illum nobis aspernatur, suscipit numquam odio quaerat aperiam magni! Aut laboriosam tempore quis placeat esse non impedi aspernatur!",
+      date: "321",
+      comments: [{ comment: 'coment-car' }, { comment: 'coment-car' }, { comment: 'coment-car' }],
+      category: 'car'
+    },
+    {
+      title: "123",
+      article:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, asperiores optio obcaecati illum nobis aspernatur, suscipit numquam odio quaerat aperiam magni! Aut laboriosam tempore quis placeat esse non impedi aspernatur!",
+      date: "321",
+      comments: [{ comment: 'coment-car' }, { comment: 'coment-car' }, { comment: 'coment-car' }],
+      category: 'car'
+    },
+    {
+      title: "123",
+      article:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, asperiores optio obcaecati illum nobis aspernatur, suscipit numquam odio quaerat aperiam magni! Aut laboriosam tempore quis placeat esse non impedi aspernatur!",
+      date: "321",
+      comments: [{ comment: 'coment-car' }, { comment: 'coment-car' }, { comment: 'coment-car' }],
+      category: 'car'
+    },
+    {
+      title: "123",
+      article:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, asperiores optio obcaecati illum nobis aspernatur, suscipit numquam odio quaerat aperiam magni! Aut laboriosam tempore quis placeat esse non impedi aspernatur!",
+      date: "321",
+      comments: [{ comment: 'coment-lifestyle' }, { comment: 'coment-lifestyle' }, { comment: 'coment-lifestyle' }],
+      category: 'lifestyle'
+    },
+  ];
+
+  export default articles;
